9453dbb3e8ea68b6bcc63305109ff51eef81e6a9
tagtart1 <adamtbizz@gmail.com>
2022-11-21 09:56:22 -0600

    Deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adam Tagtart</h1>
+      <h2>Jimmy was here</h2>
     </div>
   );
 }
